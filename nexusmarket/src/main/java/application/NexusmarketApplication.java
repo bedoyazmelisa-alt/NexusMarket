296032fe9960e4b639d9bb728a8d0b12e2fd5631
bedoyazmelisa-alt <bedoyazmelisa@gmail.com>
2026-08-27 14:25:49 -0500

    fix: application does not run

--- nexusmarket/src/main/java/application/NexusmarketApplication.java
+++ nexusmarket/src/main/java/application/NexusmarketApplication.java
@@ -1,13 +1,24 @@
 package application;
-
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
+ 
+/**
+* Application entry point. DataSource/JPA/Mongo auto-configurations are
+* excluded while the persistence adapters are not implemented yet; re-enable
+* them when the repositories are wired to MySQL/MongoDB.
+*/
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        MongoAutoConfiguration.class
+})
 public class NexusmarketApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(NexusmarketApplication.class, args);
 	}
-
 }
